4906b7da2e930e948248fb989ad6f1ab7de5d5fe
Saad1926Q <saad.ahmed1926q@gmail.com>
2026-07-22 13:10:07 +0530

    train bpe owt

---
+++ cs336_basics/bpe/train_bpe_expts_owt.py
@@ -0,0 +1,27 @@
+import pickle
+import time
+
+from cs336_basics.bpe.train import train_bpe
+
+start = time.perf_counter()
+
+
+def main():
+    vocab, merges = train_bpe(input_path="data/owt_train.txt", vocab_size=32000, special_tokens=["<|endoftext|>"])
+
+    elapsed = time.perf_counter() - start
+
+    longest_token = max(vocab.values(), key=lambda token: len(token))
+
+    with open("data/owt_bpe.pkl", "wb") as f:
+        pickle.dump({"vocab": vocab, "merges": merges}, f)
+
+    print(f"Training took {elapsed:.2f} seconds")
+    print(f"Training took {elapsed / 60:.2f} minutes")
+
+    print(f"Longest token is {longest_token!r}")
+    print(f"Length of longest token is {len(longest_token)}")
+
+
+if __name__ == "__main__":
+    main()
